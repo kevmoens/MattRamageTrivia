ab271a14898b994bea2f13e4b5c0fca2c4b30eec
Kevin Moens <kevmoens@yahoo.com>
2023-08-01 23:55:44 -0500

    initial commit

---
+++ MattRamageTrivia/Models/Contestant.cs
@@ -0,0 +1,9 @@
+﻿namespace MattRamageTrivia.Models
+{
+    public class Contestant
+    {
+        public required string Name { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public int Score { get; set; } = 0;
+    }
+}
