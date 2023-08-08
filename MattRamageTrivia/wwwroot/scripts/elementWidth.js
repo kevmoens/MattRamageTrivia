9d3f53dce522108878c60224c1332ffd4e3ae630
Kevin Moens <kevmoens@yahoo.com>
2023-08-07 19:38:22 -0500

    90% there to create hover question buttons.

---
+++ MattRamageTrivia/wwwroot/scripts/elementWidth.js
@@ -0,0 +1,27 @@
+﻿window.getElementWidth = (elementId) => {
+    let elems = document.getElementsByClassName(elementId);
+    for (var i = 0; i < elems.length; i++) {
+        return elems[i].offsetWidth;
+    }
+
+    //const element = document.getElementById(elementId);
+    //if (element) {
+    //    return element.offsetWidth;
+    //} else {
+    //    return 0;
+    //}
+};
+
+window.getElementHeight = (elementId) => {
+    let elems = document.getElementsByClassName(elementId);
+    for (var i = 0; i < elems.length; i++) {
+        return elems[i].offsetHeight;
+    }
+    //const element = document.getElementById(elementId);
+
+    //if (element) {
+    //    return element.offsetHeight;
+    //} else {
+    //    return 0;
+    //}
+};
---
+++ MattRamageTrivia/wwwroot/scripts/windowResize.js
@@ -0,0 +1,18 @@
+﻿window.handleWindowResize = (dotNetObjRef) => {
+    function onWindowResize() {
+        const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        dotNetObjRef.invokeMethodAsync('UpdateWindowSize', width, height);
+    }
+
+    // Attach the event handler
+    window.addEventListener('resize', onWindowResize);
+
+    // Call the event handler initially to set the initial window size
+    onWindowResize();
+
+    // Clean up the event handler when the component is removed
+    return () => {
+        window.removeEventListener('resize', onWindowResize);
+    };
+};
