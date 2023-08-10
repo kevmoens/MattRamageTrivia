4cd46dde75006c4f1b938581d7d1815ff1ee9938
Kevin Moens <kevmoens@yahoo.com>
2023-08-09 23:38:44 -0500

    start on game logic

--- MattRamageTrivia/wwwroot/scripts/elementWidth.js
+++ MattRamageTrivia/wwwroot/scripts/elementWidth.js
@@ -4,12 +4,6 @@
         return elems[i].offsetWidth;
     }
 
-    //const element = document.getElementById(elementId);
-    //if (element) {
-    //    return element.offsetWidth;
-    //} else {
-    //    return 0;
-    //}
 };
 
 window.getElementHeight = (elementId) => {
@@ -17,11 +11,4 @@ window.getElementHeight = (elementId) => {
     for (var i = 0; i < elems.length; i++) {
         return elems[i].offsetHeight;
     }
-    //const element = document.getElementById(elementId);
-
-    //if (element) {
-    //    return element.offsetHeight;
-    //} else {
-    //    return 0;
-    //}
 };
