beffac9fa6e3739193128f5bd14d499925934ce8
Kevin Moens <kevmoens@yahoo.com>
2023-08-25 21:08:43 -0500

    add in result correct or wrong

---
+++ MattRamageTrivia/wwwroot/scripts/videoPlayer.js
@@ -0,0 +1,24 @@
+﻿
+
+class VideoHelpers {
+    static dotNetHelper;
+
+    static setDotNetHelper(value) {
+        VideoHelpers.dotNetHelper = value;
+    }
+
+    static async videoEnded() {
+        await VideoHelpers.dotNetHelper.invokeMethodAsync('VideoEnded');
+    }
+
+}
+window.VideoHelpers = VideoHelpers;
+
+window.addVideoEndedEventListener = (videoId) =>
+{
+    var video = document.getElementById(videoId);
+    video.addEventListener("ended", async function () {
+        // Perform any action you want when the video has ended
+        await VideoHelpers.videoEnded();
+    });
+}
